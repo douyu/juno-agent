f9782a43b7251ee466a0f66b2a9f467a4563d1ae
大可 <hnlq.sysu@gmail.com>
2021-07-23 11:45:04 +0800

    feat: support region (#23)

--- pkg/proxy/regProxy/etcd/view.go
+++ pkg/proxy/regProxy/etcd/view.go
@@ -2,6 +2,8 @@ package etcd
 
 // PluginRegProxyPrometheus ETCD dataSource config
 type PluginRegProxyPrometheus struct {
-	Enable bool // 是否开启用该数据源
-	Path   string
+	Enable       bool // 是否开启用该数据源
+	Path         string
+	EnableRegion bool     // 是否开启region过滤
+	Region       []string // 指定region
 }
